f57fe02c90184fa5aa4852c0592a5c96993f6b86
Carlos Gomez <cgomez@redsis.com>
2021-01-01 15:33:56 -0500

    Final v1

---
+++ index.js
@@ -0,0 +1,44 @@
+//empezando a trabajar con node express body-parser y web-push
+
+const express = require('express')
+const webPush = require('web-push')
+const bodyparser = require('body-parser')
+const path = require('path')
+
+
+const app = express()
+
+app.use(bodyparser.json())
+
+app.use(express.static(path.join(__dirname,'public')))
+
+
+
+const publicVapidKey = 'BA74k-MUv44qCQ-0RVLwdXQrk-1tsHaPEn_hXwgJ3S7ij1SVQi1xP0a7FysQiDv1pk1TFWvglGcAKkRNwKX8kI0';
+const privateVapidKey = '6UIyL4GkjJTDEHfYqZbiwfat4Nex8OK_iZ4nBMwfJO0';
+
+webPush.setVapidDetails('mailto:carlosandres675@gmail.com',publicVapidKey,privateVapidKey);
+
+app.get('/',(req,res)=>{
+    res.send('funcionando correctamente')
+});
+
+app.post('/subscribe',(req,res)=>{
+    const subscription = req.body
+
+    res.status(201).json({});
+
+    const payload = JSON.stringify({
+        title: 'Notification desde tu backend',
+        body: 'Este es el cuerpo de la notificacion pero desde un backend'
+    });
+
+    webPush.sendNotification(subscription,payload)
+    .catch(error => console.log(error));
+});
+
+
+app.set('port', process.env.PORT || 5000);
+const server = app.listen(app.get('port'), () => {
+    console.log(`server running on ${server.address().port}`);
+})
---
+++ public/sw.js
@@ -0,0 +1,132 @@
+// self.addEventListener('install',(e)=>{
+//     console.log('SW install...');
+// });
+// self.addEventListener('activate',async(e)=>{
+//     console.log('SW Activate....');
+// });
+// self.addEventListener('fetch',(e)=>{
+//     console.log('SW Fetch....');
+// })
+
+const CACHE_NAME_CORE = 'cache-v1';
+const CACHE_FILES_CORE = [
+    'src/images/icons/icon-144x144.png',
+    'src/css/app.css',
+    "src/images/computer.jpg",
+    'src/js/app.js',
+    'src/js/firebase.js',
+    'src/js/db.js',
+    'index.html',
+    'post.html',
+    '/'
+];
+
+const CACHE_NAME_INMUTABLE = 'inmutable-v1';
+const CACHE_FILES_INMUTABLE = [
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://code.getmdl.io/1.3.0/material.amber-blue.min.css',
+    'https://code.getmdl.io/1.3.0/material.min.js',
+    'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxK.woff2',
+    "https://fonts.gstatic.com/s/materialicons/v70/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+    'https://unpkg.com/pwacompat'
+
+];
+
+const CACHE_NAME_DYNAMIC = 'Dynamic-v1';
+const CACHE_FILES_DYNAMIC = [];
+
+self.addEventListener('install',(e)=>{
+    const guardandoCache = caches.open(CACHE_NAME_CORE)
+    .then(cache =>  cache.addAll(CACHE_FILES_CORE))
+    .catch(err => console.log(err.message));
+
+    const guardandoCacheInmutable = caches.open(CACHE_NAME_INMUTABLE)
+    .then(cache =>  cache.addAll(CACHE_FILES_INMUTABLE))
+    .catch(err => console.log(err.message));
+
+    self.skipWaiting();
+    e.waitUntil(Promise.all([guardandoCache, guardandoCacheInmutable]));
+});
+
+
+self.addEventListener('activate',(e)=>{
+    //Eliminando caches obsoletos
+    const obtenercaches = caches.keys() // ['cache-v1','inmutable-v1','Dynamic-v1']
+    .then(allCaches => allCaches.filter(cache => ![CACHE_NAME_CORE,CACHE_NAME_INMUTABLE,CACHE_NAME_DYNAMIC].includes(cache)).filter(cacheName => caches.delete(cacheName)))
+    .catch(err => console.error(err.message))
+    console.log('SW Cache limpiado exitosamente');
+    e.waitUntil(obtenercaches);
+    //La documentacion nos indican eliminar CACHES anteriores
+    console.log('SW: Archivos exitosamente guardados...');
+    // e.waitUntil(clients.claim());
+});
+
+
+self.addEventListener('fetch',(e)=>{
+
+        if(!(e.request.url.indexOf('http') === 0)){
+            return;
+        }
+
+        if(e.request.url.includes('firestore.googleapis.com')){
+            return;
+        }
+
+
+        //tercera estartegia : cache pidiendo ayuda a la red
+        const cacheAyudaRed = caches.match(e.request)
+        .then(page => page || fetch(e.request)
+        .then(eventRequest =>{
+            // 
+            return caches.open(CACHE_NAME_DYNAMIC).then(cache =>{
+                if(![].concat(CACHE_FILES_CORE, CACHE_FILES_INMUTABLE).indexOf(e.request.url) || eventRequest.type === 'opaque' ){
+                    cache.put(e.request, eventRequest.clone());
+                }
+                return eventRequest;
+            })
+        }))
+        e.respondWith(cacheAyudaRed);
+});
+
+self.addEventListener('sync',(e)=>{
+    console.log('------------------------');
+    console.log(e); 
+    console.log('------------------------');
+})
+
+self.addEventListener('push',(e)=>{
+
+    // self.registration.showNotification('Hola desde push');
+    const data = e.data.json();
+    self.registration.showNotification(data.title,{
+        body:data.body,
+        icon: 'src/images/icons/icons-144x144.png',
+        image: 'src/images/computer.jpg',
+        badge: 'src/images/icons/icon-144x144.png',
+        dir: 'ltr',
+        tag: 'Notification-postme',
+        requireInteraction: true,
+        vibrate: [100, 50, 200],
+        actions: [
+            {action: 'confirm', title: 'Aceptar', icon: 'src/images/icons/icon-144x144.png'},
+            {action: 'cancel', title: 'Cancelar', icon: 'src/images/icons/icon-144x144.png'}
+        ]
+    });
+})
+
+self.addEventListener('notificationclick', (e)=>{
+    const notification = e.notification;
+    const action = e.action;
+    console.log(action);
+    if(action === 'confirm'){
+        clients.openWindow('https:www.google.com');
+        notification.close();
+    }else{
+        notification.close();
+    }
+})
+
+
+self.addEventListener('notificationclose', (e)=>{
+  console.log('Se cerro y no haremos nada');
+})
